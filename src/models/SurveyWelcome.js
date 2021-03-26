e3abd0c3bcd09336a1ea9c7ae7c59eba02baeb22
Renu Parameswaran <rparameswaran@tekion.com>
2021-03-26 14:45:31 -0700

    modifying variables

--- src/models/SurveyWelcome.js
+++ src/models/SurveyWelcome.js
@@ -1,32 +1,12 @@
 import { CONFIG_JSON_KEYS as fields } from "../constants/config-fields.js";
-import _isEmpty from "lodash/isEmpty";
-
 export default class SurveyWelcome {
   constructor(welcomeSection) {
     this.welcomeSection = welcomeSection;
-    this.data = new WeakMap();
-    this.data.set(this, {
-      text1: "",
-      text2: "",
-    });
   }
   showWelcomePage() {
-    const data = this.data.get(this);
-    let getText1 = data.text1;
-    let getText2 = data.text2;
-    getText1 = _isEmpty(this.welcomeSection[0][fields["WELCOME_TEXT"]])
-      ? ""
-      : this.welcomeSection[0][fields["WELCOME_TEXT"]];
-    getText2 = _isEmpty(this.welcomeSection[0][fields["START_TEXT"]])
-      ? "START"
-      : this.welcomeSection[0][fields["START_TEXT"]];
-    this.data.set({ getText1, getText2 });
-
-    !_isEmpty(getText1)
-      ? (document.getElementById("welcometext").innerHTML = getText1)
-      : "";
-    !_isEmpty(getText2)
-      ? (document.getElementById("buttontext").innerHTML = getText2)
-      : "";
+    const text1 = this.welcomeSection[0][fields["WELCOME_TEXT"]] || "";
+    const text2 = this.welcomeSection[0][fields["START_TEXT"]] || "";
+    document.getElementById("welcometext").innerHTML = text1;
+    document.getElementById("buttontext").innerHTML = text2;
   }
 }
